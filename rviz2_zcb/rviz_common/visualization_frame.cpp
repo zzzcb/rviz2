ca65fbb3e498fca0520e521623328be3c4cd6ab3
zcb <1056768935@qq.com>
2023-11-17 17:43:51 +0800

    [add] add center widget

--- rviz2_zcb/rviz_common/visualization_frame.cpp
+++ rviz2_zcb/rviz_common/visualization_frame.cpp
@@ -1,17 +1,22 @@
 #include "rviz_common/visualization_frame.hpp"
 
+#include <QHBoxLayout>
+
 namespace rviz_common
 {
 
 VisualizationFrame::VisualizationFrame(QWidget * parent)
   :QMainWindow(parent)
 {
-  
+    QWidget * central_widget = new QWidget(this);
+    QHBoxLayout * central_layout = new QHBoxLayout;
+    central_layout->setSpacing(0);
+    central_layout->setMargin(0);
 }
 
 VisualizationFrame::~VisualizationFrame()
 {
- 
+
 }
 
 }  // namespace rviz_common
